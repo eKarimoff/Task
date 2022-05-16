4739d60983d11f6702266ca8c5a376e314d538ff
elyorbek <elyorbekkarimov3@gmail.com>
2022-05-16 11:04:25 +0500

    master

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/material', [CompanyController::class, 'getMaterial']);
 
 Route::get('/make',[CompanyController::class,'getMaterials']);
